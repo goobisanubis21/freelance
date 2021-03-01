5c26f29d2adb988c181d84b53193cef2b7f13928
Anthony Parrino <anthony21parrino21@gmail.com>
2021-03-01 13:03:18 -0500

    styling

--- client/src/components/HomeComp/HomeComp.js
+++ client/src/components/HomeComp/HomeComp.js
@@ -10,17 +10,33 @@ function HomeComp() {
                     Full Stack Web and Mobile Responsive Applications
                 </p>
             </div>
-            <div className="card whyDiv">
-                <div className="card-body">
-                    <h2><strong>Is a Website a Necessity?</strong></h2>
-                    <p>97% of all purchase decisions start with a simple search of your website. Without a fully functioning properly developed website to demonstrate your professionalism, you or your company is only relevant to 7% of the buying market.</p>
+            <div className="whyEstimateBlock">
+                <div className="card whyDiv">
+                    <div className="card-body">
+                        <h2><strong>Is a Website a Necessity?</strong></h2>
+                        <p>97% of all purchase decisions start with a simple search of your website. Without a fully functioning properly developed website to demonstrate your professionalism, you or your company is only relevant to 7% of the buying market.</p>
+                    </div>
                 </div>
-            </div>
-            <div className="card whyDiv">
-                <div className="card-body">
-                    <h2><strong>Why WebCanvas</strong></h2>
-                    <p>We provide an affordable Full Stack Web Application Service that is mobile responsive and worthy of representing you and your business. We are avaliable for you 24 hours a day, 7 days a week to meet the requirements that, according to the market, you desprately need to ensure success. WebCanvas Development will bring your vision into fruition.</p>
+                <div className="card whyDiv">
+                    <div className="card-body">
+                        <h2><strong>Why WebCanvas</strong></h2>
+                        <p>We provide an affordable Full Stack Web Application Service that is mobile responsive and worthy of representing you and your business. We are avaliable for you 24 hours a day, 7 days a week to meet the requirements that, according to the market, you desprately need to ensure success. WebCanvas Development will bring your vision into fruition.</p>
+                    </div>
                 </div>
+                <form>
+                    <div className="card estimate">
+                        <div className="card-body">
+                            <h5>Get a Free Estimate</h5>
+                            <input type="text" placeholder="First Name" required></input>
+                            <input type="text" placeholder="Last Name" required></input>
+                            <input type="text" placeholder="Email" required></input>
+                            <input type="text" placeholder="Company Name"></input>
+                            <input type="text" placeholder="Phone Number"required></input>
+                            <textarea type="text" placeholder="Website Description" required></textarea>
+                            <button className="estimateBtn" type="submit">Submit</button>
+                        </div>
+                    </div>
+                </form>
             </div>
         </div>
     )
--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 function Nav() {
     return (
         <div>
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <nav className="navbar navbar-expand-lg navbar-light bg-light navMainDiv">
                 <Link to="/"><div className="navbar-brand">WCD</div></Link>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
