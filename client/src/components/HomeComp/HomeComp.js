1800522e1276fe01cee6e2cc8445c15c2558cd62
Anthony Parrino <anthony21parrino21@gmail.com>
2021-03-01 16:20:54 -0500

    styling

--- client/src/components/HomeComp/HomeComp.js
+++ client/src/components/HomeComp/HomeComp.js
@@ -20,23 +20,32 @@ function HomeComp() {
                 <div className="card whyDiv">
                     <div className="card-body">
                         <h2><strong>Why WebCanvas</strong></h2>
-                        <p>We provide an affordable Full Stack Web Application Service that is mobile responsive and worthy of representing you and your business. We are avaliable for you 24 hours a day, 7 days a week to meet the requirements that, according to the market, you desprately need to ensure success. WebCanvas Development will bring your vision into fruition.</p>
+                        <p>We provide an affordable Full Stack Web Application Development Service that is mobile responsive and worthy of representing you and your business. We are avaliable for you 24 hours a day, 7 days a week to meet the requirements that, according to the market, you desprately need to ensure you stay on the path of success. WebCanvas Development will bring your vision into fruition.</p>
                     </div>
                 </div>
                 <form>
                     <div className="card estimate">
                         <div className="card-body">
                             <h5>Get a Free Estimate</h5>
-                            <input type="text" placeholder="First Name" required></input>
-                            <input type="text" placeholder="Last Name" required></input>
-                            <input type="text" placeholder="Email" required></input>
-                            <input type="text" placeholder="Company Name"></input>
-                            <input type="text" placeholder="Phone Number"required></input>
-                            <textarea type="text" placeholder="Website Description" required></textarea>
+                            <label className="required">First Name</label>
+                            <input type="text" name="first_name" placeholder="First Name" required></input>
+                            <label className="required">Last Name</label>
+                            <input type="text" name="last_name" placeholder="Last Name" required></input>
+                            <label className="required">Email</label>
+                            <input type="email" name="email" placeholder="YourEmail@example.com" required></input>
+                            <label>Company Name</label>
+                            <input type="text" name="company" placeholder="Company Name"></input>
+                            <label className="required">Phone Number</label>
+                            <input type="text" name="phone_number" placeholder="Phone Number"required></input>
+                            <label className="required">Description</label>
+                            <textarea type="text" name="description" placeholder="Brief Description of your Website" required></textarea>
                             <button className="estimateBtn" type="submit">Submit</button>
                         </div>
                     </div>
                 </form>
+            </div>
+            <div className="newJersey">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/8d/New_Jersey_Counties.svg/1200px-New_Jersey_Counties.svg.png" alt="New Jersey"></img>
             </div>
         </div>
     )
