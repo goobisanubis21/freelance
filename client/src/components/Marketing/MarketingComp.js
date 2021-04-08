c1023f15b8222185952920b0303eef396c86ba78
Anthony Parrino <anthony21parrino21@gmail.com>
2021-04-08 12:21:35 -0400

    updates

--- client/src/components/Marketing/MarketingComp.js
+++ client/src/components/Marketing/MarketingComp.js
@@ -1,6 +1,9 @@
 import React, { useEffect } from 'react';
 import { Link, useLocation } from "react-router-dom";
 import "./marketing.css";
+import Gold from "./gold.png";
+import Silver from "./silver.png";
+import Bronze from "./bronze.png";
 
 function MarketingComp() {
 
@@ -14,6 +17,13 @@ function MarketingComp() {
             <div className="marketHeader">
                 <p className="marketHeaderTitle">Marketing Services</p>
                 <p className="marketHeaderText">Proven Growth for your Business</p>
+            </div>
+            <div className="fbAdsDiv">
+                <h2>The WebCanvas Marketing Packages</h2>
+                <br/>
+                <img className="fbAdsImg" src={Bronze} alt="bronze"></img>
+                <img className="fbAdsImg" src={Silver} alt="silver"></img>
+                <img className="fbAdsImg" src={Gold} alt="gold"></img>
             </div>
         </div>
     )
